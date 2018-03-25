d40042cd2bdfd11813c14eeb1789ff55788ca2de
Adam Cardenas <cevaris@gmail.com>
2018-03-24 19:54:03 -0600

    rendering clips

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import React, {Component} from 'react';
+import LazyLoad from 'react-lazyload';
 import logo from './logo.svg';
 import './App.css';
+import {getClips, handleErr} from "./index";
+import moment from 'moment';
 
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            clips: []
+        };
+
+        const ref = this;
+
+        getClips((err, docs) => {
+            if (handleErr(err)) return;
+            ref.setState({
+                clips: docs
+            });
+        });
+    }
+
+    _list() {
+        return this.state.clips.map((clip, i) => {
+            return (
+                <LazyLoad height={20} key={i} offset={100} once={true}>
+                    <div>
+                        {clip.text}: {moment.unix(clip.createdAtMs/1000).format('dddd, MMMM Do, YYYY h:mm:ss A')}
+                    </div>
+                </LazyLoad>
+            )
+        });
+    }
+
     render() {
-        console.log('hello', {hello: 'world'});
         return (
             <div className="App">
                 <header className="App-header">
@@ -14,6 +45,9 @@ class App extends Component {
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
+                <div className="list">
+                    {this._list()}
+                </div>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -9,11 +9,8 @@ const remote = window.require('electron').remote;
 const clipboard = remote.getGlobal('currentClipboard');
 let last = '';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
-
 const Datastore = require('nedb');
-export const db = new Datastore({
+const db = new Datastore({
     filename: './data.db',
     autoload: false
 });
@@ -23,26 +20,17 @@ const check_clipboard_for_changes = () => {
 
     if (current !== last && current !== '') {
         last = current;
-        console.log(current);
-        // add to list
 
         const doc = mkClip(current);
         db.insert(doc, function (err, newDoc) {
             if (handleErr(err)) return;
-            console.log(newDoc);
-
-            getClips(function (err, docs) {
-                if (handleErr(err)) return;
-                renderClips(docs);
-            });
         });
     }
 };
 
-const getClips = (func) => {
+export const getClips = (func) => {
     db.find({})
-        .sort({createdAt: -1})
-        .limit(10)
+        .sort({createdAtMs: -1})
         .exec(func);
 };
 
@@ -50,28 +38,26 @@ const mkClip = (clip) => {
     const unixMs = moment.utc().valueOf();
     return {
         text: clip,
-        createdAt: unixMs
+        createdAtMs: unixMs
     }
 };
 
-const renderClips = (clips) => {
-    console.log(clips);
-};
-
-const handleErr = (err) => {
+export const handleErr = (err) => {
     if (err) {
         console.log(err);
     }
     return err;
 };
 
+db.loadDatabase(function (err) {
+    if (handleErr(err)) return;
+    console.log('loaded db');
+});
+
 // Subscribers
 // Check for changes at an interval.
 setInterval(check_clipboard_for_changes, 200);
 
-db.loadDatabase(function (err) {
-    getClips(function (err, docs) {
-        if (handleErr(err)) return;
-        renderClips(docs);
-    });
-});
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
